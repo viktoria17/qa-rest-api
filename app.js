d1e760647066626980f9ba689b6e3b0cfcf47f61
Viktoria Gužovskaja <viktoria.guzovskaja@gmail.com>
2018-04-19 19:50:19 +0300

    Build the question routes

--- app.js
+++ app.js
@@ -2,42 +2,12 @@
 
 const express = require('express');
 const app = express();
+const routes = require('./routes');
+const jsonParser = require('body-parser').json;
 
-// localhost:3000
-app.use((req, res, next) => {
-	console.log('First piece of Middleware');
-	next();
-});
+app.use(jsonParser());
 
-// localhost:3000
-app.use((req, res, next) => {
-	console.log('Second piece of Middleware');
-	next();
-});
-
-// localhost:3000/test/1
-app.use('/test/:id', (req, res, next) => {
-	console.log('ID: ' + req.params.id);
-	next();
-});
-
-// localhost:3000
-app.use((req, res, next) => {
-	req.myMessage = 'Hi!';
-	next();
-});
-
-// localhost:3000
-app.use((req, res, next) => {
-	console.log(req.myMessage);
-	next();
-});
-
-// localhost:3000/?color=green
-app.use((req, res, next) => {
-	console.log('My favorite color is ' + req.query.color);
-	next();
-});
+app.use('/questions', routes);
 
 const port = process.env.PORT || 3000;
 

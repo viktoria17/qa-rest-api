d1a03a16396e971ccbbaa43d29c2f6b931b39c6b
Viktoria Gužovskaja <viktoria.guzovskaja@gmail.com>
2018-04-29 16:47:25 +0300

    Complete data modeling

--- models.js
+++ models.js
@@ -3,6 +3,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema; // Define a schema
 
+const sortAnswers = (a, b) => {
+    // - negative a before b,  0 no change,  + positive a after b
+    if (a.votes === b.votes) {
+        // will order the later dates first
+        return b.updatedAt - a.updatedAt;
+    }
+    // will order the larger votes first
+    return b.votes - a.votes;
+}
+
 const AnswerSchema = new Schema({
     text: String,
     createdAt: {
@@ -29,6 +39,42 @@ const QuestionSchema = new Schema({
     },
     answers: [AnswerSchema]
 
+});
+
+// When the answer is updated:
+// 1. we want to apply the updates to unanswered document
+// 2. we want the answers updated property to be the current time
+// We can call instance update method directly on the answer when we want to update it
+AnswerSchema.method('update') = function (updates, callback) {
+    // merge the updates into the answers document
+    Object.assign(this, updates, {updatedAt: new Date()});
+    // saves the parent document, the question associated with the answer;
+    // to access the question we can use the answers parent method
+    this
+        .parent()
+        .save(callback);
+}
+
+// vote instance method to help with translating strings from the URL into math
+// that moves the counts up or down
+AnswerSchema.method('vote') = function (votes, callback) {
+    if (vote === 'up') {
+        this.votes += 1;
+    } else {
+        this.votes -= 1;
+    }
+    // we need to save the parent and pass in the callback to save
+    this
+        .parent()
+        .save(callback);
+}
+
+// pre-save hook
+QuestionSchema.pre('save', function (next) {
+    this
+        .answers
+        .sort(sortAnswers); // Mongoose will sort the answers every time it is saved keeping the state of ur DB up to date
+    next();
 });
 
 // To use our schema definition, we need to convert our QuestionSchema into a
